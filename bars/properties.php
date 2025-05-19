6c108bb220d545368a646cc6f2ff9cdc72bd551c
ccadevida03 <c.cadevida03@gmail.com>
2025-05-19 14:23:03 +0800

    INITIAL COMMIT

---
+++ bars/properties.php
@@ -0,0 +1,13 @@
+<?php
+$cards = "bg-success text-white text-lg";
+$systemname = "KBF Dental Care";
+$sidebarColor = "bg-gradient-success";
+$superuser = "admins";
+$dentist = [
+    "Dr. Kathleen B. Factoriza",
+    "Dr. Kwin Elizabeth R. Factoriza",
+    "Dr. Marites San Jose"
+
+];
+
+?>
