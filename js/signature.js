836c9894a0140eb12b577777a0e27f8414e9fe0d
ccadevida03 <c.cadevida03@gmail.com>
2025-05-29 01:23:54 +0800

    updated signature performance

--- js/signature.js
+++ js/signature.js
@@ -1,34 +1,51 @@
-const modal = document.getElementById('signature-modal');
-const canvas = document.getElementById('signature-pad');
-const ctx = canvas.getContext('2d');
-
+const modalSignature = document.getElementById('signature-modal');
+const canvasSignature = document.getElementById('signature-pad');
+const ctx = canvasSignature.getContext('2d');
+ctx.strokeStyle = "#222"; // Set stroke color (black-ish)
+ctx.lineWidth = 3;
 let signatureCallback = null;
 let drawing = false;
 
 // Mouse Events
-canvas.addEventListener('mousedown', startPosition);
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', endPosition);
-canvas.addEventListener('mouseout', endPosition);
+canvasSignature.addEventListener('mousedown', startPosition);
+canvasSignature.addEventListener('mousemove', draw);
+canvasSignature.addEventListener('mouseup', endPosition);
+canvasSignature.addEventListener('mouseout', endPosition);
 
 // Touch Events
-canvas.addEventListener('touchstart', startPosition, { passive: false });
-canvas.addEventListener('touchmove', draw, { passive: false });
-canvas.addEventListener('touchend', endPosition);
+canvasSignature.addEventListener('touchstart', startPosition, { passive: false });
+canvasSignature.addEventListener('touchmove', draw, { passive: false });
+canvasSignature.addEventListener('touchend', endPosition);
 
+
+
+function resizeCanvas() {
+    const rect = canvasSignature.getBoundingClientRect();
+    canvasSignature.width = rect.width;
+    canvasSignature.height = rect.height;
+    ctx.strokeStyle = "#222";
+    ctx.lineWidth = 5;
+
+}
 function getXY(e) {
+    const rect = canvasSignature.getBoundingClientRect();
+    let clientX, clientY;
+
     if (e.touches && e.touches.length > 0) {
-        const rect = canvas.getBoundingClientRect();
-        return {
-            x: e.touches[0].clientX - rect.left,
-            y: e.touches[0].clientY - rect.top
-        };
+        clientX = e.touches[0].clientX;
+        clientY = e.touches[0].clientY;
+    } else if (e.changedTouches && e.changedTouches.length > 0) {
+        clientX = e.changedTouches[0].clientX;
+        clientY = e.changedTouches[0].clientY;
     } else {
-        return {
-            x: e.offsetX,
-            y: e.offsetY
-        };
+        clientX = e.clientX;
+        clientY = e.clientY;
     }
+
+    return {
+        x: clientX - rect.left,
+        y: clientY - rect.top
+    };
 }
 
 function startPosition(e) {
@@ -45,29 +62,38 @@ function draw(e) {
     const pos = getXY(e);
     ctx.lineTo(pos.x, pos.y);
     ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(pos.x, pos.y);
 }
 
 function endPosition(e) {
+    if (drawing) {
+        ctx.closePath();
+    }
     drawing = false;
 }
 
 
 function clearPad() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvasSignature.width, canvasSignature.height);
 }
 
 function openSignatureModal(callback) {
     signatureCallback = callback;
     clearPad();
-    modal.style.display = "flex";
+    modalSignature.style.display = "flex";
+    setTimeout(() => {
+        resizeCanvas();
+
+    }, 10);
 }
 
 function closeSignatureModal() {
-    modal.style.display = "none";
+    modalSignature.style.display = "none";
 }
 
 function confirmSignature() {
-    const dataURL = canvas.toDataURL('image/png');
+    const dataURL = canvasSignature.toDataURL('image/png');
     if (typeof signatureCallback === "function") {
         signatureCallback(dataURL);
     }
