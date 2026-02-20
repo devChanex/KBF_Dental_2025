011daab9adbdfd2a874d688fbfa5fe031e350fd6
ccadevida03 <c.cadevida03@gmail.com>
2026-02-20 10:53:28 +0800

    Updated sql host

--- services/databaseService.php
+++ services/databaseService.php
@@ -10,7 +10,7 @@ class Database
     // private $username = "root";
     // private $password = '';
 
-    private $host = "216.218.206.40";
+    private $host = "localhost";
     private $db_name = "kbfdenta_sysdb";
     private $username = "kbfdenta_sysadmin";
     private $password = '$ysAdmin2022';
